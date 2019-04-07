1323e1da70a23bd108f1251296d4515324d040b0
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-04-07 15:34:43 +0200

    feat(filter-svg): add common filter icon component

--- src/app/mobile/components/BottomNavigation/Tab/tab-styled.tsx
+++ src/app/mobile/components/BottomNavigation/Tab/tab-styled.tsx
@@ -2,12 +2,21 @@ import styled from 'styled-components';
 import CommonText from 'common/Text/Text';
 import CommonHomeIcon from 'common/Svg/Home/Home-svg';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  flex: 1;
+  justify-content: center;
+  padding: 0 0 5px 0;
+`;
 
 const Text = styled(CommonText)`
   text-transform: uppercase;
+  align-self: center;
 `;
 
-const HomeIcon = styled(CommonHomeIcon)``;
+const HomeIcon = styled(CommonHomeIcon)`
+  align-self: center;
+`;
 
 export { Wrapper, Text, HomeIcon };
--- src/app/mobile/views/BottomNavigation/BottonNavigation-views.tsx
+++ src/app/mobile/views/BottomNavigation/BottonNavigation-views.tsx
@@ -6,6 +6,9 @@ import Tab from 'mobile/components/BottomNavigation/Tab/Tab-component';
 const BottomNavigation: FunctionComponent = (): JSX.Element => (
   <Container>
     <Tab />
+    <Tab />
+    <Tab />
+    <Tab />
   </Container>
 );
 
