49966af9a423bb2fd525b07d34a0b83bb812704f
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-04-07 10:13:26 +0200

    refactor(header-mobile): change file name to Header-view

--- src/app/mobile/App-mobile.tsx
+++ src/app/mobile/App-mobile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import HeaderView from 'mobile/views/Header/Header-component';
+import HeaderView from 'mobile/views/Header/Header-view';
 import { Container } from './app-styled';
 import { GlobalStyled } from 'globalStyled/app-styled';
 
