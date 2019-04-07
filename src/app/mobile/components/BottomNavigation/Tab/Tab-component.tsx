308bef554544925b0a7a9460dbc4c8cef362b140
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-04-07 13:16:49 +0200

    feat(bottomNav-mobile): add wrapper for Tab component

---
+++ src/app/mobile/components/BottomNavigation/Tab/Tab-component.tsx
@@ -0,0 +1,7 @@
+import React, {FunctionComponent} from 'react';
+
+import { Wrapper } from './tab-styled';
+
+const Tab: FunctionComponent = () => <Wrapper>fddfdfdf</Wrapper>;
+
+export default Tab;
--- src/app/mobile/views/BottomNavigation/BottonNavigation-views.tsx
+++ src/app/mobile/views/BottomNavigation/BottonNavigation-views.tsx
@@ -1,7 +1,12 @@
 import React, { FunctionComponent } from 'react';
 
 import { Container } from './bottomNavigation-styled';
+import Tab from 'mobile/components/BottomNavigation/Tab/Tab-component';
 
-const BottomNavigation: FunctionComponent = (): JSX.Element => <Container />;
+const BottomNavigation: FunctionComponent = (): JSX.Element => (
+  <Container>
+    <Tab />
+  </Container>
+);
 
 export default BottomNavigation;
