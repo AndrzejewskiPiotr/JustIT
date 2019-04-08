54d879c4f2f4e605101e8da9098991607fdd1492
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-04-08 19:47:26 +0200

    feat(routes): add simple routing for test

--- src/app/mobile/App-mobile.tsx
+++ src/app/mobile/App-mobile.tsx
@@ -4,12 +4,29 @@ import HeaderView from 'mobile/views/Header/Header-view';
 import BottomNavigation from 'mobile/views/BottomNavigation/BottonNavigation-views';
 import { Container } from './app-styled';
 import { GlobalStyled } from 'globalStyled/app-styled';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+const Elo = () => <div>test</div>;
 
 const App = (): JSX.Element => (
   <Container>
     <GlobalStyled />
-    <HeaderView />
-    <BottomNavigation />
+    <Router>
+      <HeaderView />
+      <Switch>
+        <Route path="/" component={Elo} />
+        <Route path="/favourites" component={Elo} />
+        <Route path="/languages" component={Elo} />
+        <Route path="/map" component={Elo} />
+        <Redirect from="*" to="/" />
+      </Switch>
+      <BottomNavigation />
+    </Router>
   </Container>
 );
 
