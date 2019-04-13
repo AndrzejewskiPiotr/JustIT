ba077601646cdb2be9936d59aea9da6a817ce587
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-04-13 17:31:19 +0200

    feat(jobList): add jobList container

--- src/app/mobile/App-mobile.tsx
+++ src/app/mobile/App-mobile.tsx
@@ -4,6 +4,7 @@ import { thunkSendMessage } from 'redux/jobs/jobs-actions';
 import { connect } from 'react-redux';
 import HeaderView from 'mobile/views/Header/Header-view';
 import BottomNavigation from 'mobile/views/BottomNavigation/BottonNavigation-views';
+import JobsList from 'mobile/views/JobsList/JobsList-view';
 import { Container } from './App-styled';
 import { GlobalStyled } from 'globalStyled/app-styled';
 import {
@@ -13,7 +14,7 @@ import {
   Switch,
 } from 'react-router-dom';
 
-const Elo = () => <div>test</div>;
+const Elo = () => <JobsList />;
 const Heyd = () => <div>favourites</div>;
 const Heyf = () => <div>languages</div>;
 const Hey = () => <div>map</div>;
---
+++ src/app/mobile/views/JobsList/JobsList-view.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from './JobsList-styled';
+
+const JobsList = () => <Container>jobsList</Container>;
+
+export default JobsList;
