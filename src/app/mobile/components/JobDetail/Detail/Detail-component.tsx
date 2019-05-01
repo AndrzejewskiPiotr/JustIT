36e1d8406c4c256a6cea4d45352168490b2b447e
PiotrAndrzejewski <piotr.andrzejewski@profil-software.com>
2019-05-01 16:11:04 +0200

    refactor(job-detail): props destructuring, fix tab titles

--- src/app/mobile/components/JobDetail/Detail/Detail-component.tsx
+++ src/app/mobile/components/JobDetail/Detail/Detail-component.tsx
@@ -11,33 +11,43 @@ import {
 import JobHeading from 'mobile/components/JobDetail/JobHeading/JobHeading-component';
 import Skills from 'mobile/components/JobDetail/Skills/Skills-component';
 
-const Detail: FunctionComponent<any> = ({ data }) => (
-  <Container>
-    <JobHeading title={data.title} />
-    <Wrapper>
-      <Row>
-        <CompanyName text={data.company_name} heading="Company Name" />
-        <CompanySize text={data.company_size} heading="Company Size" />
-      </Row>
-      <Row>
-        <CompanyEmploymentType text={data.employment_type} heading="EMP.type" />
-        <CompanyExperience text={data.experience_level} heading="EMP.lvl" />
-      </Row>
-      <Row>
-        <CompanyEmploymentType
-          text={`${data.street}, ${data.city}`}
-          heading="address"
-        />
-        <CompanyExperience
-          text={`${data.salary_from} - ${data.salary_from} ${
-            data.salary_currency
-          }`}
-          heading="EMP.lvl"
-        />
-      </Row>
-      <Skills title="Required skills" skills={data.skills} />
-    </Wrapper>
-  </Container>
-);
+const Detail: FunctionComponent<any> = ({ data }) => {
+  const {
+    title,
+    company_name,
+    company_size,
+    employment_type,
+    experience_level,
+    city,
+    street,
+    salary_from,
+    salary_to,
+    salary_currency,
+    skills,
+  } = data;
+  return (
+    <Container>
+      <JobHeading title={title} />
+      <Wrapper>
+        <Row>
+          <CompanyName text={company_name} heading="Company Name" />
+          <CompanySize text={company_size} heading="Company Size" />
+        </Row>
+        <Row>
+          <CompanyEmploymentType text={employment_type} heading="EMP.type" />
+          <CompanyExperience text={experience_level} heading="EXP.lvl" />
+        </Row>
+        <Row>
+          <CompanyEmploymentType text={`${street}, ${city}`} heading="address" />
+          <CompanyExperience
+            text={`${salary_from} - ${salary_to} ${salary_currency}`}
+            heading="Salary"
+          />
+        </Row>
+        <Skills title="Required skills" skills={skills} />
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Detail;
